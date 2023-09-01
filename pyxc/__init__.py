911884146405e923430ea44549bdba72c58acc04
Tuna Lee <10000685+polarities@users.noreply.github.com>
2023-09-01 17:37:31 +0200

    Reformatting __init__.py

--- pyxc/__init__.py
+++ pyxc/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "1.0.0"
-__name__ = 'pyxc'
-__author__ = 'Sang-Hyeok Lee, Mattis Seehaus'
+__name__ = "pyxc"
+__author__ = "Sang-Hyeok Lee, Mattis Seehaus"
 __description__ = "Python point-to-point correlation library."
